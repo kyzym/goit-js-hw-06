162cc244ded3e8e89e2e555d3f518938f8713fb5
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-14 16:41:46 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+
+const elementsArray = ingredients.map((item) => {
+  const createEl = document.createElement("li");
+  createEl.classList.add("item");
+  createEl.textContent = item;
+
+  return createEl;
+});
+
+list.append(...elementsArray);
