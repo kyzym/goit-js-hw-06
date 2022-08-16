4594f5789a56c93e670932e25eb45c9c29fdd0c4
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-16 16:02:55 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const onForm = document.querySelector(".login-form");
+onForm.addEventListener("submit", validation);
 
+function validation(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (!password.value || !email.value) {
+    alert("Empty field! 😬 Write something");
+  }
+
+  const formData = { email: email.value, password: password.value };
+
+  console.log(formData);
+  onForm.reset();
+}
