8d8753e003ef665b53457f56839dc16aa98b200e
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-17 18:00:23 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,6 +1,6 @@
 const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-textInput.addEventListener("keydown", (event) => {
+textInput.addEventListener("input", (event) => {
   output.textContent = event.currentTarget.value;
 });
