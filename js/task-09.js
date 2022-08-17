3e1edb8ec87a3340a5dbfc33f6043767a773daa0
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-17 07:19:54 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const onBody = document.querySelector("body");
+const onChangeBtn = document.querySelector(".change-color");
+const onColorText = document.querySelector(".color");
+
+function changeColor() {
+  onBody.style.backgroundColor = getRandomHexColor();
+  onColorText.textContent = getRandomHexColor();
+}
+
+onChangeBtn.addEventListener("click", changeColor);
