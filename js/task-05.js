29e3ad2a5f49262a811037fa5cc245000a66ebcf
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-19 17:24:41 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,4 +3,7 @@ const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
   output.textContent = event.currentTarget.value;
+  if (!textInput.value.length) {
+    output.textContent = "Anonymous";
+  }
 });
