2f93a77c35ee94562f9488370e3eb6b9f0ad29f7
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-08-19 16:25:24 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,12 @@ const input = document.querySelector("#validation-input");
 
 function validate() {
   const { value, dataset, classList } = input;
-  value.length !== Number(dataset.length)
-    ? classList.add("invalid")
-    : classList.remove("invalid");
-  classList.add("valid");
+  if (value.length !== Number(dataset.length)) {
+    classList.add("invalid");
+  } else {
+    classList.remove("invalid");
+    classList.add("valid");
+  }
 }
 
 input.addEventListener("blur", validate);
